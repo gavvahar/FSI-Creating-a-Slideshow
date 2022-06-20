8e7fe86d7da42140f4655380740aa73c5a7a706f
gavvahar <nihargavva@gmail.com>
2022-06-20 19:11:39 -0400

    finished but still need to shorten up the code

--- index.js
+++ index.js
@@ -13,42 +13,38 @@ const previousButton = document.querySelector(".previous");
 const bubbleButtons = document.querySelectorAll(".bubble");
 
 let currentImageIndex = 0;
-image.src = images[currentImageIndex]
+image.src = images[currentImageIndex];
 bubbleButtons[currentImageIndex].textContent = "●";
 
-for(let i = 0; i < bubbleButtons.length; i++)
-{
-    bubbleButtons[i].addEventListener("click", function (e)
-    {
-        currentImageIndex = i;
-        image.src =images[currentImageIndex]
-        for(let i = 0; i <  bubbleButtons.length; i++)
-        {
-            bubbleButtons[i].textContent = "○";
-        }
-        bubbleButtons[currentImageIndex].textContent = "●";
-    })
-}
-
-previousButton.addEventListener("click", function()
-{
-    if(currentImageIndex > 0)
-    {
-        currentImageIndex--;
-    }
-    image.src = images[currentImageIndex]
-    for(let i = 0; i < bubbleButtons.length; i++)
-    {
-        bubbleButtons[i].textContent = "○";
+for (let i = 0; i < bubbleButtons.length; i++) {
+  bubbleButtons[i].addEventListener("click", function (e) {
+    currentImageIndex = i;
+    image.src = images[currentImageIndex];
+    for (let i = 0; i < bubbleButtons.length; i++) {
+      bubbleButtons[i].textContent = "○";
     }
     bubbleButtons[currentImageIndex].textContent = "●";
-})
+  });
+}
 
-nextButton.addEventListener("click", function()
-{
-    if(currentImageIndex < 5)
-    {
-        currentImageIndex++;
-    }
-    image.src = images[currentImageIndex]
-})
+previousButton.addEventListener("click", function () {
+  if (currentImageIndex > 0) {
+    currentImageIndex--;
+  }
+  image.src = images[currentImageIndex];
+  for (let i = 0; i < bubbleButtons.length; i++) {
+    bubbleButtons[i].textContent = "○";
+  }
+  bubbleButtons[currentImageIndex].textContent = "●";
+});
+
+nextButton.addEventListener("click", function () {
+  if (currentImageIndex < 5) {
+    currentImageIndex++;
+  }
+  image.src = images[currentImageIndex];
+  for (let i = 0; i < bubbleButtons.length; i++) {
+    bubbleButtons[i].textContent = "○";
+  }
+  bubbleButtons[currentImageIndex].textContent = "●";
+});
